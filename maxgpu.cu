86ad5f2d478c55c21c9dd446fb6094eac2c6740a
jtxtina <yanglq08@gmail.com>
2019-05-01 23:03:38 -0400

    second version lab3 parallel computing

--- maxgpu.cu
+++ maxgpu.cu
@@ -5,24 +5,48 @@
 
 #define THREADS_PER_BLOCK 1024
 
+unsigned int getmax(unsigned int *, unsigned int);
 unsigned int getmaxcu(unsigned int *, unsigned int);
 
-__global__void getmaxCUDA1(unsigned int * num_d, int new_size, unsigned int * block_result) {
-  extern __shared__ unsigned int local_num[];
+void printDeviceInfo()
+{
+    int nDevices;
+    cudaGetDeviceCount(&nDevices);
+
+    for (int i = 0; i < nDevices; i++)
+    {
+        struct cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+        printf("Device Number: %d\n", i);
+        printf("  Device name: %s\n", prop.name);
+        printf("  Memory Clock Rate (KHz): %d\n",
+               prop.memoryClockRate);
+        printf("  Memory Bus Width (bits): %d\n",
+               prop.memoryBusWidth);
+        printf("  Peak Memory Bandwidth (GB/s): %f\n\n",
+               2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+    }
+} // End of the print device info function
+
+
+
+
+__global__ void getmaxCUDA1(unsigned int * num_d, int new_size, unsigned int * block_result) {
+  __shared__ unsigned int local_num[THREADS_PER_BLOCK];
   int tid = threadIdx.x;
   int thread_id = blockDim.x * blockIdx.x + threadIdx.x;
 
   //Grab the array from the global memory to shared memory.(In one block)
-  local_num[tid] = num_d[thread_id];
-  __syncthreads();
+   local_num[tid] = 0;
+   if(thread_id < new_size) {
+    local_num[tid] = num_d[thread_id];
+   }
+   __syncthreads();
 
-  for(int i = blockDim.x; i > 1 ; i = i / 2) {
-
-    int stride = blockDim.x / 2;
-
+  for(int i = (blockDim.x)/2; i >= 1 ; i = i / 2) {
+    int stride = i;
     if(tid < stride && (tid + stride) < blockDim.x) {
-
-      if(local_num[tid < local_num[tid+stride]) {
+      if(local_num[tid] < local_num[tid+stride]) {
         local_num[tid] = local_num[tid+stride];
       }
     }
@@ -37,9 +61,7 @@ __global__void getmaxCUDA1(unsigned int * num_d, int new_size, unsigned int * bl
 
 }
 
-__global__void getmaxCUDA2() {
 
-}
 
 int main(int argc, char *argv[])
 {
@@ -63,14 +85,19 @@ int main(int argc, char *argv[])
        exit(1);
     }
 
-    srand(time(NULL)); // setting a seed for the random number generator
-    // Fill-up the array with random numbers from 0 to size-1
-    for( i = 0; i < size; i++)
-       numbers[i] = rand()  % size;
+    printDeviceInfo();
 
+
+   srand(time(NULL)); // setting a seed for the random number generator
+    // Fill-up the array with random numbers from 0 to size-1
+    for( i = 0; i < size; i++){
+       numbers[i] = rand()  % size;
+//       printf("number[%d] is %d\n",i, numbers[i]);
+    }
     printf(" The maximum number in the array is: %u\n",
            getmaxcu(numbers, size));
-
+    printf(" The maximum number by sequential  in the array is: %u\n",
+           getmax(numbers, size));
     free(numbers);
     exit(0);
 }
@@ -83,8 +110,8 @@ int main(int argc, char *argv[])
 */
 unsigned int getmaxcu(unsigned int num[], unsigned int size)
 {
-  unsigned int i;
   unsigned int max = num[0];
+ // printf("max[5] is %d max [6] is %d max[1] is %d\n",num[5], num[6], num[1]);
 
   unsigned int* num_d;
   unsigned int* block_result;
@@ -93,6 +120,7 @@ unsigned int getmaxcu(unsigned int num[], unsigned int size)
   if(size % THREADS_PER_BLOCK != 0) {
     block_num = block_num + 1;
   }
+  unsigned int block_count = block_num;
   int sizen = size*sizeof(unsigned int);
   int sizeb = block_num*sizeof(unsigned int);
   int new_size = size;
@@ -117,18 +145,28 @@ unsigned int getmaxcu(unsigned int num[], unsigned int size)
     }
     num_d = block_result;
   }
+  result = (unsigned int*) malloc(block_count * sizeof(unsigned int));
   cudaMemcpy(result, block_result, sizeb, cudaMemcpyDeviceToHost);
 
 
-  printf("max is %d", result[0]);
   max = result[0];
   //3. Free device memory for num[].
   cudaFree(num_d);
   cudaFree(block_result);
 
-  /*for(i = 1; i < size; i++)
+
+  return( max );
+
+}
+
+unsigned int getmax(unsigned int num[], unsigned int size)
+{
+  unsigned int i;
+  unsigned int max = num[0];
+
+  for(i = 1; i < size; i++)
 	if(num[i] > max)
-	   max = num[i];*/
+	   max = num[i];
 
   return( max );
 
